5d65413d630cc152de82a326025711b1f1572d02
professionalcodeguy <kaue.g.p2015@gmail.com>
2024-09-06 12:25:14 +0000

    06-09-2

---
+++ app/src/main/java/application/model/Pessoa.java
@@ -0,0 +1,29 @@
+package application.model;
+
+public class Pessoa {
+    private long id;
+    private String nome;
+    private int idade;
+
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
